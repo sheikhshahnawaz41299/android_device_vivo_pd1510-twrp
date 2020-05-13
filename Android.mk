3696cf9dd8b63093233ebd6f107cdc0874b4b2ce
sheikhshahnawaz41299 <sheikhshahnawaz5555@gmail.com>
2020-05-14 01:32:40 +0530

    pd1510: initial twrp 9.0

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@
 # Copyright (C) 2016 The Android Open Source Project
 # Copyright (C) 2016 The TWRP Open Source Project
 # Copyright (C) 2020 SebaUbuntu's TWRP device tree generator 
+# Copyright (C) 2020 the_shahnawaz_sheikh (sheikhshahnawaz41299)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
